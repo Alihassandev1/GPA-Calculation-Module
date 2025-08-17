3eb7fe7dcd2f0830a5110f54fdc6784c959b6ac9
Alihassandev1 <alihassan6266249@gmail.com>
2025-08-17 13:39:24 +0500

    Add files via upload

---
+++ GPACalculator.py
@@ -0,0 +1,77 @@
+studentData = {
+            # // Students with MATH-110 (3 CH)
+    "24131519-003": {"name": "ABDUL REHMAN", "oop": 64, "dld": 77, "ew": 68, "ds": 76, "math": 50, "is": 78,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-004": {"name": "ANAS MAJEED", "oop": 80, "dld": 91, "ew": 76, "ds": 64, "math": 57, "is": 87,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-005": {"name": "ABDUL BASIT", "oop": 62, "dld": 65, "ew": 71, "ds": 78, "math": 56, "is": 83,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-006": {"name": "MUHAMMAD SHOAIB", "oop": 63, "dld": 64, "ew": 73, "ds": 76, "math": 45, "is": 89,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-007": {"name": "TALHA SHABBIR", "oop": 49, "dld": 78, "ew": 76, "ds": 68, "math": 58, "is": 89,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-009": {"name": "ARHAM", "oop": 51, "dld": 70, "ew": 74, "ds": 85, "math": 70, "is": 88,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-010": {"name": "SAQIB ALI", "oop": 51, "dld": 71, "ew": 65, "ds": 80, "math": 65, "is": 88,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-011": {"name": "MUNEEB ALI", "oop": 61, "dld": 88, "ew": 76, "ds": 69, "math": 54, "is": 88,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-012": {"name": "FURQAN", "oop": 67, "dld": 80, "ew": 67, "ds": 60, "math": 55, "is": 88,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-013": {"name": "MUHAMMAD TAHA RASHEED", "oop": 67, "dld": 79, "ew": 73, "ds": 61, "math": 57, "is": 90,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-014": {"name": "MIRZA SUBHAN TAYIB", "oop": 66, "dld": 68, "ew": 63, "ds": 64, "math": 50, "is": 86,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-015": {"name": "DAWOOD AHMAD", "oop": 62, "dld": 72, "ew": 66, "ds": 75, "math": 50, "is": 86,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-018": {"name": "ALI HUSNAIN", "oop": 63, "dld": 86, "ew": 74, "ds": 80, "math": 57, "is": 88,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-019": {"name": "ABDULLAH ASIF", "oop": 48, "dld": 49, "ew": 59, "ds": 74, "math": 60, "is": 73,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-020": {"name": "HANZLA AMANAT", "oop": 49, "dld": 49, "ew": 65, "ds": 76, "math": 65, "is": 70,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-021": {"name": "ABDULLAH IMRAN", "oop": 48, "dld": 39, "ew": 68, "ds": 67, "math": 70, "is": 74,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-027": {"name": "HASNAT AKRAM", "oop": 61, "dld": 79, "ew": 67, "ds": 85, "math": 80, "is": 82,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-028": {"name": "AHSAN IQBAL", "oop": 52, "dld": 72, "ew": 71, "ds": 77, "math": 70, "is": 80,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-030": {"name": "ALI HASSAN", "oop": 61, "dld": 89, "ew": 72, "ds": 86, "math": 77, "is": 89,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-031": {"name": "SYED ZAIN ALI", "oop": 48, "dld": 90, "ew": 65, "ds": 80, "math": 75, "is": 68,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-032": {"name": "KASHAN SAEED", "oop": 40, "dld": 66, "ew": 58, "ds": 78, "math": 49, "is": 77,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-033": {"name": "JUNAID HUSSAIN", "oop": 63, "dld": 90, "ew": 68, "ds": 90, "math": 58, "is": 92,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-035": {"name": "HAMZA QAMAR", "oop": 59, "dld": 75, "ew": 57, "ds": 80, "math": 60, "is": 88,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-036": {"name": "MUHAMMAD HASSAN AAMAR", "oop": 57, "dld": 79, "ew": 70, "ds": 53, "math": 59, "is": 89,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-037": {"name": "ADIL SHAHZAD", "oop": 57, "dld": 83, "ew": 73, "ds": 66, "math": 63, "is": 84,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-038": {"name": "ABDUL QADIR", "oop": 38, "dld": 80, "ew": 70, "ds": 85, "math": 73, "is": 86,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-039": {"name": "SHAMIKH SHAFIQ", "oop": 38, "dld": 76, "ew": 57, "ds": 82, "math": 75, "is": 80,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-041": {"name": "FATIMA TUL ZAHRA", "oop": 72, "dld": 78, "ew": 75, "ds": 82, "math": 60, "is": 89,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-042": {"name": "AIMAN KAALEEM", "oop": 64, "dld": 79, "ew": 79, "ds": 73, "math": 67, "is": 86,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-043": {"name": "NIMRA KHURSHID", "oop": 66, "dld": 90, "ew": 72, "ds": 85, "math": 52, "is": 85,"totalCH" : 18, "mathType": "MATH-110"},
+    "24131519-044": {"name": "MAHNOOR BAKHT", "oop": 61, "dld": 80, "ew": 74, "ds": 84, "math": 65, "is": 84,"totalCH" : 18, "mathType": "MATH-110"},
+
+    # // Students with MATH-128 (0 CH) - excluded from SGPA calculation
+    "24131519-008": {"name": "MUHAMMAD ABDULLAH", "oop": 49, "dld": 60, "ew": 75, "ds": 80, "math": 40, "is": 76,"totalCH" : 15, "mathType": "MATH-128"},
+    "24131519-016": {"name": "FAHAD SAEED", "oop": 45, "dld": 64, "ew": 72, "ds": 84, "math": 40, "is": 90,"totalCH" : 15, "mathType": "MATH-128"},
+    "24131519-017": {"name": "MUHAMMAD ABU BAKAR CHEEMA", "oop": 60, "dld": 66, "ew": 72, "ds": 79, "math": 40, "is": 88,"totalCH" : 15, "mathType": "MATH-128"},
+    "24131519-024": {"name": "AMAN ULLAH SHAHID", "oop": 55, "dld": 76, "ew": 67, "ds": 82, "math": 40, "is": 85,"totalCH" : 15, "mathType": "MATH-128"},
+    "24131519-040": {"name": "FAIQA SHAHZAD", "oop": 65, "dld": 85, "ew": 78, "ds": 91, "math": 53, "is": 88,"totalCH" : 15, "mathType": "MATH-128"},
+    "24131519-045": {"name": "HIFZA ZAFAR", "oop": 66, "dld": 89, "ew": 79, "ds": 90, "math": 43, "is": 91,"totalCH" : 15, "mathType": "MATH-128"},
+    "24131519-046": {"name": "IZZA", "oop": 75, "dld": 91, "ew": 73, "ds": 93, "math": 48, "is": 85,"totalCH" : 15, "mathType": "MATH-128"},
+    "24131519-047": {"name": "MOMNA MEHMOOD", "oop": 77, "dld": 88, "ew": 82, "ds": 77, "math": 60, "is": 95,"totalCH" : 15, "mathType": "MATH-128"},
+    "24131519-048": {"name": "SYED USAMA MAQSOOD", "oop": 59, "dld": 67, "ew": 69, "ds": 79, "math": 40, "is": 80,"totalCH" : 15, "mathType": "MATH-128"}
+}
+
+# GPA scale according to UOG
+def gpaScale(num):
+    if num >= 84.5: return 4.00
+    elif num >= 79.5: return 3.70 
+    elif num >= 74.5: return 3.50
+    elif num >= 69.5: return 3.00
+    elif num >= 64.5: return 2.50
+    elif num >= 59.5: return 2.00 
+    elif num >= 54.5: return 1.50
+    elif num >= 49.5: return 1.00
+    else: return 0.00
+
+# Credit Hours of Calculus Students
+creditHoursEng = [4, 3, 3, 3, 3, 2]
+# Credit Hours of Pre-Calculus Students
+creditHoursPre = [4, 3, 3, 3, 0, 2]
+
+# Calculation of GPA of every Student
+def calculateGPA(studentData, creditHoursEng, creditHoursPre):
+    for std in studentData.values():
+        tempChSum = 0
+        count = 0
+        for i in ["oop", "dld", "ew", "ds", "math", "is"]:
+            if std["mathType"] == "pre":
+                tempChSum += gpaScale(std[i]) * creditHoursPre[count]
+            else:
+                tempChSum += gpaScale(std[i]) * creditHoursEng[count]
+            count += 1
+        std["gpa"] = round(tempChSum / std["totalCH"], 1)
+    else:
+        return studentData
